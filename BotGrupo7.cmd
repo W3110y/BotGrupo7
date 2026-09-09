68c80655c4b5889e6d93ae9762c4aedf3faf9e82
W3110y <wjiaohu005@gmail.com>
2026-09-09 10:59:46 +0200

    introducior datos del bot

---
+++ BotGrupo7.cmd
@@ -0,0 +1,2 @@
+@echo off
+python BotGrupo7.py
---
+++ BotGrupo7.py
@@ -0,0 +1,26 @@
+from BotInRedUC3M import BotInRedUC3M
+from robocode_tank_royale.bot_api.events import ScannedBotEvent
+
+class BotGrupo7(BotInRedUC3M):
+
+    def __init__(self):
+        super().__init__("BotGrupo7.json") # vuestro .json
+        # Lógica principal: se repite cada turno de la ronda
+        def run(self):
+            while self.running:
+                self.set_turn_radar_right(45) # barrer el radar SIEMPRE
+                self.go() # cierra el turno
+        # Os han escaneado a un rival -> aquí va vuestro targeting / movimiento
+
+    def on_scanned_bot(self, e: ScannedBotEvent):
+        self.set_fire(1)
+        # (Opcional) lógica por turno
+
+    def on_tick(self, e):
+        pass
+
+    def main():
+        BotGrupo7().start()
+
+    if __name__ == "__main__":
+        main()
