1f9eca6b931855b3c322255e9dac5b0cc122bc34
W3110y <wjiaohu005@gmail.com>
2026-09-09 11:03:03 +0200

    arreglar programa principal

--- BotGrupo7.py
+++ BotGrupo7.py
@@ -5,13 +5,14 @@ class BotGrupo7(BotInRedUC3M):
 
     def __init__(self):
         super().__init__("BotGrupo7.json") # vuestro .json
-        # Lógica principal: se repite cada turno de la ronda
-        def run(self):
-            while self.running:
-                self.set_turn_radar_right(45) # barrer el radar SIEMPRE
-                self.go() # cierra el turno
-        # Os han escaneado a un rival -> aquí va vuestro targeting / movimiento
+        
+    # Lógica principal: se repite cada turno de la ronda
+    def run(self):
+        while self.running:
+            self.set_turn_radar_right(45) # barrer el radar SIEMPRE
+            self.go() # cierra el turno
 
+    # Os han escaneado a un rival -> aquí va vuestro targeting / movimiento
     def on_scanned_bot(self, e: ScannedBotEvent):
         self.set_fire(1)
         # (Opcional) lógica por turno
@@ -19,8 +20,8 @@ class BotGrupo7(BotInRedUC3M):
     def on_tick(self, e):
         pass
 
-    def main():
-        BotGrupo7().start()
+def main():
+    BotGrupo7().start()
 
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
