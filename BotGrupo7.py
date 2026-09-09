b3cf2559a02e86e2af721e121129042fe47e8d80
W3110y <wjiaohu005@gmail.com>
2026-09-09 11:44:47 +0200

    introducir mas metodos y configurar json

--- BotGrupo7.py
+++ BotGrupo7.py
@@ -1,5 +1,6 @@
 from BotInRedUC3M import BotInRedUC3M
 from robocode_tank_royale.bot_api.events import ScannedBotEvent
+from robocode_tank_royale.bot_api.events import HitByBulletEvent
 
 class BotGrupo7(BotInRedUC3M):
 
@@ -10,13 +11,22 @@ class BotGrupo7(BotInRedUC3M):
     def run(self):
         while self.running:
             self.set_turn_radar_right(45) # barrer el radar SIEMPRE
+            self.forward(100)
+            self.turn_gun_left(360)
+            self.back(100)
+            self.turn_gun_left(360)
             self.go() # cierra el turno
 
     # Os han escaneado a un rival -> aquí va vuestro targeting / movimiento
     def on_scanned_bot(self, e: ScannedBotEvent):
         self.set_fire(1)
-        # (Opcional) lógica por turno
 
+    def on_hit_by_bullet(self, e: HitByBulletEvent):
+        self.set_turn_left(100)
+        self.set_turn_right(100)
+
+
+    # (Opcional) lógica por turno
     def on_tick(self, e):
         pass
 
